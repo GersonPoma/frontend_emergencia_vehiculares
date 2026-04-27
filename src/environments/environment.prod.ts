355ddf6c5cb09815adc1e00d8df615ae14065d81
Gerson <villarroelpoma2@gmail.com>
2026-04-27 08:44:18 -0400

    cambios

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://backend-emergencia-vehiculares.onrender.com',
+  googleMapsApiKey: 'AIzaSyCXdws6gbso34U2o1KqpUnSZlnW3URK8-Q',
+  vapidKey: 'BFqwiDlxRijlQ5hNBsRXFILuxdiBDAyOo6XucexX1-GpKLPR_jYpxOwmkdMwqXSvcLr1Ra3gNe4sFzvaajchYx8',
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCfR-CBfloYqS9w3ZGsKZoi3USgeSTa2SM",
+    authDomain: "emergencias-vehiculares-app.firebaseapp.com",
+    projectId: "emergencias-vehiculares-app",
+    storageBucket: "emergencias-vehiculares-app.firebasestorage.app",
+    messagingSenderId: "318737587315",
+    appId: "1:318737587315:web:05199f12b573816585de06"
+  }
+};
